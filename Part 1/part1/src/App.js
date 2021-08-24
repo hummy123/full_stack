a292532de153a71226358570f59e22c460a2f60f
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-08-24 15:04:25 +0100

    finished 1.4

--- Part 1/part1/src/App.js
+++ Part 1/part1/src/App.js
@@ -2,24 +2,31 @@ import React from 'react'
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
+
+    let total = 0
+    for (const part of parts) {
+        total += part.exercises
     }
 
     return (
       <>
           <Header course={course} />
-          <Content p1={part1} p2={part2} p3={part3} />
-          <Total sum={part1.exercises + part2.exercises + part3.exercises} />
+          <Content parts={parts} />
+          <Total sum={total} />
       </>
   )
 }
@@ -31,11 +38,10 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    const parts = props.parts.map(part => <Part key={part.name} part={part} /> )
     return (
         <>
-            <Part object={props.p1} />
-            <Part object={props.p2} />
-            <Part object={props.p3} />
+            {parts}
         </>
     )
 }
@@ -48,7 +54,7 @@ const Total = (props) => {
 
 const Part = (props) => {
     return (
-        <p>{props.object.name} {} {props.object.exercises}</p>
+        <p>{props.part.name} {props.part.exercises}</p>
     )
 }
 
