c7c613d4627c8da11179c178d3516d5a25cfa4ae
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-08-31 12:34:29 +0100

    finished 2.6

---
+++ Part 2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName
+        }
+        setPersons(persons.concat(personObject))
+        setNewName("")
+    }
+
+    const enterName = (event) => {
+        setNewName(event.target.value)
+    }
+
+  return (
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input value={newName} onChange={enterName} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        <ul>
+            {persons.map(person =>
+                <Person key={person.name} person={person} />
+            )}
+        </ul>
+      </div>
+  )
+}
+
+export default App
