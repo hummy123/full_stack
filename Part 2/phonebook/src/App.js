cb1c6ec90eb8b06635ca8e7f9b9a2a8aef418366
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-08-31 12:49:57 +0100

    finished 2.8

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -3,13 +3,16 @@ import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: "07835 209170" }
   ])
   const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
+
 
     const addPerson = () => {
         const personObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         }
         setPersons(persons.concat(personObject))
         setNewName("")
@@ -27,6 +30,10 @@ const App = () => {
         setNewName(event.target.value)
     }
 
+    const enterNumber = (event) => {
+        setNewNumber(event.target.value)
+    }
+
   return (
       <div>
         <h2>Phonebook</h2>
@@ -34,6 +41,9 @@ const App = () => {
           <div>
             name: <input value={newName} onChange={enterName} />
           </div>
+            <div>
+                number: <input value={newNumber} onChange={enterNumber} />
+            </div>
           <div>
             <button type="submit">add</button>
           </div>
--- Part 2/phonebook/src/components/Person.js
+++ Part 2/phonebook/src/components/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Person = (props) => {
-    return <li>{props.person.name}</li>
+    return <li>Name: {props.person.name} Number: {props.person.number}</li>
 }
 
 export default Person
