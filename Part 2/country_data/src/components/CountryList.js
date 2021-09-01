0a9d593c4f7cc2ff4388931b73447f8d676ae2b3
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-09-01 14:49:35 +0100

    finished 2.13

--- Part 2/country_data/src/components/CountryList.js
+++ Part 2/country_data/src/components/CountryList.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import CountryDetailed from "./CountryDetailed";
 
 const CountryList = ({countries}) => {
-    return(<><h2>Countries</h2>
-        <ul>
-            {countries.map(country =>
-                <li key={country.name}> {country.name} </li>
-            )}
-        </ul>
-    </>)
+
+    const showDetail = (country) => <CountryDetailed country={country} />
+
+    const showList = () => {
+        return(
+            <>
+                <h2>Countries</h2>
+                <ul>
+                    {countries.map(country =>
+                        <li key={country.name}>
+                            {country.name}
+                            <button
+                                onClick={() => setDisplay(showDetail(country))}>
+                                Show
+                            </button>
+                        </li>
+                    )}
+                </ul>
+            </>
+        )
+    }
+
+    const [display, setDisplay] = useState(showList)
+
+    return(display)
 }
 
 export default CountryList
