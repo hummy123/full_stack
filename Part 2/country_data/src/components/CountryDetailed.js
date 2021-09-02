07c7a939c8396b46e7852d6e2f596a7af0ac2a59
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-09-02 11:38:02 +0100

    finished 2.13

--- Part 2/country_data/src/components/CountryDetailed.js
+++ Part 2/country_data/src/components/CountryDetailed.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const CountryDetailed = ({country}) => {
-    console.log(country)
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const weatherAPI = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${country.capital}`
+
+    const [weather, setWeather] = useState('')
+
+    useEffect(() => {
+        axios.get(weatherAPI)
+            .then(response => {
+                setWeather(response.data.current)
+            })
+    }, [weatherAPI])
+
+
     return(
         <>
             <h2>{country.name}</h2>
@@ -15,6 +28,13 @@ const CountryDetailed = ({country}) => {
                 )}
             </ul>
             <img src={country.flag} alt={`The flag of ${country.name}`} />
+            <h2>Weather in {country.capital}</h2>
+            <img
+                src={weather.weather_icons}
+                alt={`The weather in ${country.capital} is ${weather.weather_descriptions}`}
+            />
+            <p>{weather.weather_descriptions}</p>
+            <p>Temperature: {weather.temperature}</p>
         </>
     )
 }
