5ad96e6dda484e9389bb2a8309cf066e27175cb5
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-09-11 22:43:58 +0100

    Refactor part 3.

--- Part 3/phonebook-fullstack/backend/models/models.mjs
+++ Part 3/phonebook-fullstack/backend/models/models.mjs
@@ -1,100 +1,94 @@
 import mongoose from 'mongoose'
 import uniqueValidator from 'mongoose-unique-validator'
+import logger from '../utils/logger.mjs'
+import {MONGODB_URI} from '../utils/config.mjs'
 
-class Models {
-	//schema model
-	static Contact = mongoose.model('Contact', new mongoose.Schema({
-		name: {
-			type: String,
-			minLength: 3,
-			required: true,
-			unique: true
-		},
-		number: {
-			type: String,
-			minLength: 8,
-			required: true,
-		}
-	}).set('toJSON', {
-		transform: (document, returnedObject) => {
-			returnedObject.id = returnedObject._id.toString()
-			delete returnedObject._id
-			delete returnedObject.__v
-		}
-	}).plugin(uniqueValidator))
-
-	static url = ''
-
-	static async setUrl (url) {
-		this.url = url
+//schema model
+const contactModel = mongoose.model('Contact', new mongoose.Schema({
+	name: {
+		type: String,
+		minLength: 3,
+		required: true,
+		unique: true
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		required: true,
 	}
-
-	static async connect () {
-		//connect to db
-		try {
-			await mongoose.connect(this.url)
-			console.log('connected to db')
-		} catch (err) {
-			console.log('error connecting to db:', err.message)
-		}
+}).set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
 	}
+}).plugin(uniqueValidator))
 
-	//function to save contact
-	static async save (name, number) {
-		//connect to db
-		await this.connect()
-
-		//create contact object according to schema
-		const contact = new this.Contact({
-			name: name,
-			number: number
-		})
-
-		//save
-		const result = await contact.save()
-		await this.close()
-		return result
-	}
-
-	//function to get all
-	static async getAll() {
-		await this.connect()
-		const result = await this.Contact.find({})
-		await this.close()
-		return result
-	}
-
-	//function to get single contact by id
-	static async find(id) {
-		await this.connect()
-		const result = await this.Contact.findById(id)
-		await this.close()
-		return result
-	}
-
-	//function to close db
-	static async close() {
-		await mongoose.connection.close()
-	}
-
-	static async delete(id) {
-		await this.connect()
-		const result = await this.Contact.findByIdAndRemove(id)
-		await this.close()
-		return result
-	}
-
-	static async update(id, number) {
-		await this.connect()
-
-		const result = await this.Contact.findByIdAndUpdate(id,
-			{number: number},
-			{new: true,
-				runValidators: true})
-
-		await this.close()
-		return result
+const connect = async () => {
+	//connect to db
+	try {
+		await mongoose.connect(`${MONGODB_URI}`)
+		logger.info('connected to db')
+	} catch (err) {
+		logger.error('error connecting to db:', err.message)
 	}
 }
 
-export default Models
+//function to save contact
+const save = async (name, number) => {
+	//connect to db
+	await connect()
+
+	//create contact object according to schema
+	const contact = new contactModel({
+		name: name,
+		number: number
+	})
+
+	//save
+	const result = await contact.save()
+	await close()
+	return result
+}
+
+//function to get all
+const getAll = async () => {
+	await connect()
+	const result = await contactModel.find({})
+	await close()
+	return result
+}
+
+//function to get single contact by id
+const find = async (id) => {
+	await connect()
+	const result = await contactModel.findById(id)
+	await close()
+	return result
+}
+
+//function to close db
+const close = async () => {
+	await mongoose.connection.close()
+}
+
+const remove = async (id) => {
+	await connect()
+	const result = await contactModel.findByIdAndRemove(id)
+	await close()
+	return result
+}
+
+const update = async (id, number) => {
+	await connect()
+
+	const result = await contactModel.findByIdAndUpdate(id,
+		{number: number},
+		{new: true,
+			runValidators: true})
+
+	await close()
+	return result
+}
+
+export default {getAll, save, find, update, remove}
